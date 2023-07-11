e8f357daa7d219748977728f9f941dc4b814a78e
POWFIX <powfix@gmail.com>
2023-07-11 21:24:19 +0900

    forgot build

--- dist/src/services/Session.js
+++ dist/src/services/Session.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,7 +37,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Session = void 0;
 const events_1 = __importDefault(require("events"));
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const jose = __importStar(require("jose"));
 const logWithTs = (...p) => {
     console.log(Date.now(), ...p);
 };
@@ -38,7 +61,7 @@ class Session {
         this.hasAuthorization = () => !!this.getAuthorization();
         this.setSession = (token) => __awaiter(this, void 0, void 0, function* () {
             token = token.replace(/^Bearer\s+/, '');
-            const decoded = jsonwebtoken_1.default.decode(token);
+            const decoded = jose.decodeJwt(token);
             if (!decoded) {
                 throw new Error('failed to decode');
             }

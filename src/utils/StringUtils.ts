1916445a62b842525d27317606967a16461e7537
POWFIX <powfix@gmail.com>
2023-09-22 17:13:26 +0900

    문자열의 byte 수를 계산하는 함수 추가(ASCII 0~127이면 1바이트, 그 외는 2바이트)

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,5 +1,17 @@
 export class StringUtils {
-	static numberWithCommas(x: string | any): string {
+	public static numberWithCommas(x: string | any): string {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+
+	public static getByte(s: string): number {
+		const getByteLength = (decimal: number) => {
+			const LINE_FEED = 10;
+			return (decimal >> 7) || (LINE_FEED === decimal) ? 2 : 1
+		};
+
+		return s
+			.split('')
+			.map((s) => s.charCodeAt(0))
+			.reduce((prev, unicodeDecimalValue) => prev + getByteLength(unicodeDecimalValue), 0)
 	}
 }

b0423107042a9eeafdfb2ba65238b24239faf450
POWFIX <powfix@gmail.com>
2023-10-24 18:40:55 +0900

    0.5.3

--- dist/src/utils/StringUtils.d.ts
+++ dist/src/utils/StringUtils.d.ts
@@ -1,4 +1,5 @@
 export declare class StringUtils {
     static numberWithCommas(x: string | any): string;
     static getByte(s: string): number;
+    static levenshteinDistance: (str1?: string, str2?: string) => number;
 }

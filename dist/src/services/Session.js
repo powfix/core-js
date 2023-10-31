01a906d13e495b980a7330f0b621aca5aa3eedfe
POWFIX <powfix@gmail.com>
2023-10-31 21:09:53 +0900

    0.5.8

--- dist/src/services/Session.js
+++ dist/src/services/Session.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -33,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Session = void 0;
-const jose = __importStar(require("jose"));
+const jwt_decode_1 = require("jwt-decode");
 const logWithTs = (...p) => {
     console.log(Date.now(), ...p);
 };
@@ -89,9 +66,9 @@ class Session {
             try {
                 // Replace Bearer prefix
                 nextAuthorization = nextAuthorization.replace(/^Bearer\s+/, '');
-                console.log(`before jose.decodeJwt("${nextAuthorization}")`);
-                const decoded = jose.decodeJwt(nextAuthorization);
-                console.log(`after jose.decodeJwt("${nextAuthorization}")`);
+                console.log(`before jwtDecode("${nextAuthorization}")`);
+                const decoded = (0, jwt_decode_1.jwtDecode)(nextAuthorization);
+                console.log(`after jwtDecode("${nextAuthorization}")`);
                 if (!decoded) {
                     console.warn('decode failed');
                     return null;

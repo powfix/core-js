094a66e036acb36d5c9cdc3b1b84c8689c0c024b
POWFIX <powfix@gmail.com>
2023-11-01 09:39:39 +0900

    0.5.9

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,24 @@
 /** @constants */
-import { DURATION } from "./src/constants/DURATION";
-import { DISTANCE } from "./src/constants/DISTANCE";
+import {DURATION} from "./src/constants/DURATION";
+import {DISTANCE} from "./src/constants/DISTANCE";
 /** @model */
-import { Coordinate, CoordinateM } from "./src/models/Coordinate";
-import { Point2 } from "./src/models/Point2";
-import { Point3 } from "./src/models/Point3";
+import {Coordinate, CoordinateM} from "./src/models/Coordinate";
+import {Point2} from "./src/models/Point2";
+import {Point3} from "./src/models/Point3";
 /** @service */
-import { Session, SessionOptions, StorageProvider } from "./src/services/Session";
+import {Session, SessionOptions, StorageProvider} from "./src/services/Session";
 /** @utils */
-import { between } from "./src/utils/global/between";
-import { StringUtils } from "./src/utils/StringUtils";
-import { UuidUtils } from "./src/utils/UuidUtils";
-import { ArrayUtils } from "./src/utils/ArrayUtils";
-import { parseBoolean } from "./src/utils/BooleanUtils";
-import { CoordinateUtils } from "./src/utils/CoordinateUtils";
-import { DateUtils } from "./src/utils/DateUtils";
-import { RandomUtils } from "./src/utils/RandomUtils";
-import { Sequence, Sequencer, SequencerEvent, SequencerOption, SequencerStatus } from "./src/utils/Sequencer";
-import { Validator } from './src/utils/Validator';
+import {between} from "./src/utils/global/between";
+import {StringUtils} from "./src/utils/StringUtils";
+import {UuidUtils} from "./src/utils/UuidUtils";
+import {ArrayUtils} from "./src/utils/ArrayUtils";
+import {parseBoolean} from "./src/utils/BooleanUtils";
+import {CoordinateUtils} from "./src/utils/CoordinateUtils";
+import {DateUtils} from "./src/utils/DateUtils";
+import {RandomUtils} from "./src/utils/RandomUtils";
+import {Sequence, Sequencer, SequencerEvent, SequencerOption, SequencerStatus} from "./src/utils/Sequencer";
+import {Validator} from './src/utils/Validator';
+import {base64Polyfill} from "./src/scripts/base64-polyfill";
 /** @constants */
 export { DISTANCE, DURATION, };
 /** @models */
@@ -26,3 +27,4 @@ export { Coordinate, CoordinateM, Point2, Point3, };
 export { Session, SessionOptions, StorageProvider, };
 /** @Utils */
 export { between, UuidUtils, ArrayUtils, RandomUtils, DateUtils, StringUtils, CoordinateUtils, parseBoolean, Validator, Sequence, SequencerStatus, SequencerOption, SequencerEvent, Sequencer, };
+export { base64Polyfill, };

0cca3a26e853fdca510f643e8cc0754d0f530505
POWFIX <powfix@gmail.com>
2023-11-28 15:27:58 +0900

    Cleanup

--- index.ts
+++ index.ts
@@ -5,8 +5,6 @@ export * from './browser';
 import {RedisClient} from "./src/services/redis/RedisClient";
 import {RedisPublisher} from "./src/services/redis/RedisPublisher";
 import {RedisSubscriber} from "./src/services/redis/RedisSubscriber";
-
-// services
 export {
   RedisClient,
   RedisPublisher,

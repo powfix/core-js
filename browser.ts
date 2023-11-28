1b844a17e8528a965fe07801b365d7a61a67f95a
POWFIX <powfix@gmail.com>
2023-11-28 15:27:31 +0900

    Cleanup

--- browser.ts
+++ browser.ts
@@ -1,14 +1,36 @@
 /** @constants */
 import {DURATION} from "./src/constants/DURATION";
 import {DISTANCE} from "./src/constants/DISTANCE";
+export {
+  DISTANCE,
+  DURATION,
+};
+
 /** @model */
 import {Coordinate, CoordinateM} from "./src/models/Coordinate";
 import {Point2} from "./src/models/Point2";
 import {Point3} from "./src/models/Point3";
+export {
+  Coordinate,
+  CoordinateM,
+  Point2,
+  Point3,
+};
+
 /** @service */
 import {Session, SessionOptions, StorageProvider} from "./src/services/Session";
+export {
+  Session,
+  SessionOptions,
+  StorageProvider,
+};
+
 /** @types */
 import {IntRange} from "./src/types/IntRage";
+export {
+  IntRange,
+};
+
 /** @utils */
 import {between} from "./src/utils/global/between";
 import {StringUtils} from "./src/utils/StringUtils";
@@ -22,29 +44,6 @@ import {Validator} from './src/utils/Validator';
 import {JuminNumberUtils} from "./src/utils/JuminNumberUtils";
 import {Sequence, Sequencer, SequencerEvent, SequencerOption, SequencerStatus} from "./src/utils/Sequencer";
 import {base64Polyfill} from "./src/scripts/base64-polyfill";
-
-export {
-  DISTANCE,
-  DURATION,
-};
-
-export {
-  Coordinate,
-  CoordinateM,
-  Point2,
-  Point3,
-};
-
-export {
-  Session,
-  SessionOptions,
-  StorageProvider,
-};
-
-export {
-  IntRange,
-};
-
 export {
   between,
   base64Polyfill,

ab4b56acf783a31fda5bbd415982edfdb800dd9f
POWFIX <powfix@gmail.com>
2023-11-30 16:45:19 +0900

    Add isValidCoordinate(), crowDistance take two params

--- src/utils/CoordinateUtils.ts
+++ src/utils/CoordinateUtils.ts
@@ -1,24 +1,30 @@
-export class CoordinateUtils {
-  public static isValidLatitudeLongitude(latitude: number | string, longitude: number | string) {
-    return CoordinateUtils.isValidLatitude(latitude) && CoordinateUtils.isValidLongitude(longitude);
-  }
+import {Coordinate} from "../interfaces/Coordinate";
 
-  public static isValidLatitude(latitude: number | string) {
+export class CoordinateUtils {
+  public static isValidLatitude(latitude: Coordinate['latitude'] | string) {
     return /^-?([0-8]?[0-9]|90)(\.[0-9]{1,10})$/.test(latitude.toString());
   }
 
-  public static isValidLongitude(longitude: number | string) {
+  public static isValidLongitude(longitude: Coordinate['longitude'] | string) {
     return /^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$/.test(longitude.toString());
   }
 
-  public static crowDistance(latitude1: number, longitude1: number, latitude2: number, longitude2: number): number {
+  public static isValidLatitudeLongitude(latitude: Coordinate['latitude'] | string, longitude: Coordinate['longitude'] | string) {
+    return CoordinateUtils.isValidLatitude(latitude) && CoordinateUtils.isValidLongitude(longitude);
+  }
+
+  public static isValidCoordinate(coordinate: Coordinate): boolean {
+    return this.isValidLatitudeLongitude(coordinate.latitude, coordinate.longitude);
+  }
+
+  public static crowDistance(c1: Coordinate, c2: Coordinate): number {
     const toRad = (value: number) => value * Math.PI / 180;
 
     const R = 6371e3;
-    const dLat = toRad(latitude2 - latitude1);
-    const dLon = toRad(longitude2 - longitude1);
-    const lat1 = toRad(latitude1);
-    const lat2 = toRad(latitude2);
+    const dLat = toRad(c2.latitude - c1.latitude);
+    const dLon = toRad(c2.longitude - c1.longitude);
+    const lat1 = toRad(c1.latitude);
+    const lat2 = toRad(c2.latitude);
 
     const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);

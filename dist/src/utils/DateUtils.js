37f4bf9d7a0492db3476f263a9b66562d941017f
POWFIX <powfix@gmail.com>
2024-05-09 18:00:31 +0900

    0.9.4

--- dist/src/utils/DateUtils.js
+++ dist/src/utils/DateUtils.js
@@ -63,10 +63,10 @@ DateUtils.getRelativeTimeSpec = (locale) => {
             yy: number => `${number} years`
         },
         ja: {
-            future: "%s 後",
-            past: "%s 前",
+            future: "%s後",
+            past: "%s前",
             s: number => `${number}秒`,
-            ss: number => `${number}초`,
+            ss: number => `${number}秒`,
             m: number => `${number}分`,
             mm: number => `${number}分`,
             h: number => `${number}時`,
@@ -79,8 +79,8 @@ DateUtils.getRelativeTimeSpec = (locale) => {
             yy: number => `${number}年`
         },
         zh: {
-            future: "%s 后",
-            past: "%s 前",
+            future: "%s后",
+            past: "%s前",
             s: number => `${number}秒`,
             ss: number => `${number}秒`,
             m: number => `${number}分钟`,

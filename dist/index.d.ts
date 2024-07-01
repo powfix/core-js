733172bb503ae0b74be427042098af5a7f77bf97
a7591 <a7591@zeroweb.kr>
2024-07-01 17:50:30 +0900

    test

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,2 @@
-export * from "./src/constants";
-export * from "./src/interfaces";
-export * from "./src/scripts";
+export * from "./browser";
 export * from "./src/services";
-export * from "./src/types";
-export * from "./src/utils";

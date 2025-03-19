9602e096d2437e094f7ad38a4c493e6ad406458e
a7591 <a7591@zeroweb.kr>
2025-03-19 10:17:11 +0900

    feat: Added getPrimaryUuidColumn and getForeignUuidColumn functions to SequelizeUtils

--- src/utils/backend/base/SequelizeUtils.ts
+++ src/utils/backend/base/SequelizeUtils.ts
@@ -1,3 +1,6 @@
+import {ModelAttributeColumnOptions} from "sequelize";
+import {UuidUtils} from "../../UuidUtils";
+
 export class SequelizeUtils {
   public static decimal2Number(value: any): number | null | undefined {
     if (value === null || value === undefined) {
@@ -10,4 +13,54 @@ export class SequelizeUtils {
     }
     return parsed;
   }
+
+  public static getPrimaryUuidColumn = (): Partial<ModelAttributeColumnOptions> => ({
+    type: "binary(16)",
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+    defaultValue: () => UuidUtils.toBuffer(UuidUtils.v4()),
+    get() {
+      return UuidUtils.toString(this.getDataValue("uuid"));
+    },
+    set(uuid: string) {
+      this.setDataValue("uuid", UuidUtils.toBuffer(uuid));
+    }
+  })
+
+  public static getForeignUuidColumn = ({columnName, allowNull}: {
+    columnName: string,
+    allowNull: boolean
+  }): Partial<ModelAttributeColumnOptions> => {
+    if (allowNull) {
+      return {
+        type: "binary(16)",
+        allowNull,
+        get() {
+          const value = this.getDataValue(columnName)
+
+          if (value === null) {
+            return value
+          }
+
+          return UuidUtils.toString(this.getDataValue(columnName));
+        },
+        set(uuid: string | null) {
+          this.setDataValue(columnName, uuid === null ? null : UuidUtils.toBuffer(uuid));
+        }
+      }
+    } else {
+      return {
+        type: "binary(16)",
+        allowNull,
+        get() {
+          return UuidUtils.toString(this.getDataValue(columnName));
+        },
+        set(uuid: string) {
+          this.setDataValue(columnName, UuidUtils.toBuffer(uuid));
+        }
+      }
+    }
+  }
+
 }

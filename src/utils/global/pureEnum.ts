9a58af39a98fb9fbab65d9b4c9bcef01304568cf
a7591 <a7591@zeroweb.kr>
2025-03-25 17:17:40 +0900

    feat: Added typescript support for pureEnum function

--- src/utils/global/pureEnum.ts
+++ src/utils/global/pureEnum.ts
@@ -1,3 +1,10 @@
+type NonFunction<T, Type extends "keys" | "values"> = {
+  [K in keyof T]: T[K] extends Function ? never : Type extends "keys" ? K : T[K];
+} [keyof T] extends infer R ? (R extends never ? never : R) : never;
+
+type PureEnumReturnType<E extends object> = [NonFunction<E, "keys">, NonFunction<E, "values">][];
+
 export function pureEnum<E extends object>(e: E) {
-  return Object.entries(e).filter(([key, value]) => !/^\d+$/g.test(key) && typeof value !== 'function');
+  return Object.entries(e).filter(([key, value]) => !/^\d+$/g.test(key) && typeof value !== 'function') as PureEnumReturnType<E>;
 }
+

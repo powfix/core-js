8f7f2944d9afe4ff0d1e8ad9befbce094d4e6f35
POWFIX <powfix@gmail.com>
2025-04-03 15:58:56 +0900

    Add flush & execute function

--- src/managers/TransactionManager.ts
+++ src/managers/TransactionManager.ts
@@ -14,7 +14,7 @@ class TransactionManager {
 
   // Members
   private logLevel: TransactionManagerLogLevel = TransactionManagerLogLevel.VERBOSE;
-  private transactionTimeoutMap = new Map<Transaction, NodeJS.Timeout>();
+  private transactionTimeoutMap = new Map<Transaction, {handler: NodeJS.Timeout, action: TransactionManagerTimeoutAction}>();
 
   // Emitter
   private readonly emitter = new EventEmitter<TransactionManagerEventTypes>();
@@ -61,7 +61,10 @@ class TransactionManager {
     const action = option?.timeoutAction ?? TransactionManager.DEFAULT_ACTION;
     const timeout: number = option?.timeout ?? TransactionManager.DEFAULT_TIMEOUT;
     const handler = setTimeout(this.timeout.bind(this), timeout, transaction, action);
-    this.transactionTimeoutMap.set(transaction, handler);
+    this.transactionTimeoutMap.set(transaction, {
+      handler,
+      action,
+    });
 
     // Callback
     transaction.afterCommit(this.afterCommit.bind(this));
@@ -70,7 +73,7 @@ class TransactionManager {
   public remove(transaction: Transaction) {
     const transactionTimeout = this.transactionTimeoutMap.get(transaction);
     if (transactionTimeout != null) {
-      clearTimeout(transactionTimeout);
+      clearTimeout(transactionTimeout.handler);
       this.transactionTimeoutMap.delete(transaction);
 
       if (this.logLevel >= TransactionManagerLogLevel.VERBOSE) {
@@ -79,14 +82,29 @@ class TransactionManager {
     }
   }
 
-  private async timeout(transaction: Transaction, action: TransactionManagerTimeoutAction) {
+  public flush(action?: TransactionManagerTimeoutAction) {
+    return Promise.allSettled(
+      Array.from(this.transactionTimeoutMap.entries()).map(async ([transaction, transactionTimeout]) => {
+        const handled = await this.execute(transaction, action ?? transactionTimeout.action, 'flush');
+        this.remove(transaction);
+        return {
+          handled,
+          transaction,
+        };
+      })
+    );
+  }
+
+  private async execute(transaction: Transaction, action: TransactionManagerTimeoutAction, reason: string): Promise<boolean> {
+    let handled: boolean = false;
+
     const finished: unknown = (transaction as any)?.finished;
     if (finished != null) {
       if (this.logLevel >= TransactionManagerLogLevel.VERBOSE) {
-        console.log(LOG_TAG, this.getTransactionLogArg(transaction), `is already handled(${finished}) after timeout`);
+        console.log(LOG_TAG, this.getTransactionLogArg(transaction), `is already handled(${finished}) after`, reason);
       }
       this.remove(transaction);
-      return;
+      return handled;
     }
 
     try {
@@ -110,22 +128,28 @@ class TransactionManager {
         }
       }
 
-      const handled: boolean = finished != null;
+      handled = finished != null;
       if (finished != null) {
         if (this.logLevel >= TransactionManagerLogLevel.ERROR) {
-          console.error(LOG_TAG, this.getTransactionLogArg(transaction), `handled(${finished}) after timeout`);
+          console.error(LOG_TAG, this.getTransactionLogArg(transaction), `handled(${finished}) after`, reason);
         }
       } else {
         if (this.logLevel >= TransactionManagerLogLevel.ERROR) {
           console.error(LOG_TAG, this.getTransactionLogArg(transaction), `not handled 🚫`);
         }
       }
-
-      // Emit
-      this.emitter.emit('onUnhandledTransaction', transaction, handled);
     } catch (e) {
       console.error(e);
+    } finally {
+      this.remove(transaction);
     }
+
+    return handled;
+  }
+
+  private async timeout(transaction: Transaction, action: TransactionManagerTimeoutAction) {
+    const handled = await this.execute(transaction, action, 'timeout');
+    this.emitter.emit('onUnhandledTransaction', transaction, handled);
   }
 
   private afterCommit(transaction: Transaction) {

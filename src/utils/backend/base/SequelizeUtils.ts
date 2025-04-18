8f3308d130e1033be6bba30fc855f9d71fc472e4
a7591 <a7591@zeroweb.kr>
2025-04-18 15:39:28 +0900

    feat: Removed getNullArrayFilter

--- src/utils/backend/base/SequelizeUtils.ts
+++ src/utils/backend/base/SequelizeUtils.ts
@@ -1,6 +1,5 @@
-import {ModelAttributeColumnOptions, Op, WhereOptions} from "sequelize";
+import {ModelAttributeColumnOptions} from "sequelize";
 import {UuidUtils} from "../../UuidUtils";
-import {NOT_NULL} from "../../../constants";
 
 interface UuidColumnOptions extends Omit<ModelAttributeColumnOptions, 'type'>{
   columnName: string,
@@ -125,19 +124,5 @@ export class SequelizeUtils {
         ...overrideOptions
       }
     }
-  }
-
-  public static getNullableArrayFilter<T=undefined>(arr: (null | any)[]) {
-    return {
-      [Op.or]: arr.map(value => {
-        if (value === null) {
-          return {[Op.is]: value}
-        } else if (value === NOT_NULL) {
-          return {[Op.not]: null}
-        } else {
-          return {[Op.eq]: value}
-        }
-      })
-    } as WhereOptions<T>
   }
 }

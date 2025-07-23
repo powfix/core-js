36c8a3009ceac9fa21f377f23c4da3a1d403cb5e
POWFIX <powfix@gmail.com>
2025-07-23 19:24:10 +0900

    lowercase

--- src/utils/UuidUtils.ts
+++ src/utils/UuidUtils.ts
@@ -12,10 +12,10 @@ export class UuidUtils {
 	static format(uuid: string) {
 		if (uuid.length === 32) {
 			// Without dash: ca23c587d7f84c76be59f53bbc9f91f8
-			return `${uuid.substring(0, 8)}-${uuid.substring(8, 12)}-${uuid.substring(12, 16)}-${uuid.substring(16, 20)}-${uuid.substring(20, 32)}`.toUpperCase();
+			return `${uuid.substring(0, 8)}-${uuid.substring(8, 12)}-${uuid.substring(12, 16)}-${uuid.substring(16, 20)}-${uuid.substring(20, 32)}`.toLowerCase();
 		} else if (uuid.length === 36) {
 			// With dash: ca23c587-d7f8-4c76-be59-f53bbc9f91f8
-			return uuid.toUpperCase();
+			return uuid.toLowerCase();
 		} else {
 			// Unexpected uuid
 			console.warn('Unexpected uuid length', uuid);

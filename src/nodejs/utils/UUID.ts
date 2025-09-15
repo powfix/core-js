763e61577ecca01171a3487025368f7d3bcdf215
POWFIX <powfix@gmail.com>
2025-09-15 18:58:10 +0900

    for test

--- src/nodejs/utils/UUID.ts
+++ src/nodejs/utils/UUID.ts
@@ -1,7 +1,7 @@
 import {UUID as BaseUUID} from "../../shared/utils/UUID";
 
 export class UUID extends BaseUUID {
-  public toBuffer(): Buffer {
-    return Buffer.from(this.bytes);
-  }
+  // public toBuffer(): Buffer {
+  //   return Buffer.from(this.bytes);
+  // }
 }

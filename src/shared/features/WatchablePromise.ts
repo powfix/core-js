98cb2808c0e8b070f2bfbdd9cc7d849e478e34f2
POWFIX <powfix@gmail.com>
2025-09-30 14:02:02 +0900

    Fix error, add from

--- src/shared/features/WatchablePromise.ts
+++ src/shared/features/WatchablePromise.ts
@@ -1,21 +1,26 @@
 import type {WatchablePromiseStatus} from "./WatchablePromise.types";
 
 export class WatchablePromise<T, E = Error> extends Promise<T> {
+  public static from<P extends typeof WatchablePromise, T>(this: P, promise: Promise<T>): InstanceType<P> {
+    return new this<T>((resolve, reject) => promise.then(resolve, reject)) as InstanceType<P>;
+  }
+
   #settled: boolean = false;
   #status: WatchablePromiseStatus = 'pending';
   #result: T | PromiseLike<T> | E | undefined = undefined;
 
   constructor(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
-    super(executor);
     super((resolve, reject) => {
       executor(
         (value) => {
           resolve(value);
+          this.#settled = true;
           this.#status = 'fulfilled';
           this.#result = value;
         },
         (reason: E) => {
           reject(reason);
+          this.#settled = true;
           this.#status = 'rejected';
           this.#result = reason;
         },

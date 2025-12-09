56f2c4bdf8676788f16292b4cc0fdda721c71e02
POWFIX <powfix@gmail.com>
2025-12-09 15:18:03 +0900

    Update XOR util type, e.g. XOR<[T1, T2, T3 ...]>

--- src/shared/types/XOR.ts
+++ src/shared/types/XOR.ts
@@ -1,3 +1,32 @@
-type Without<T, U = never> = { [P in keyof T]?: U }
+export type XOR<T extends any[]> = XORBuild<
+  T,
+  XORCollectKeys<T, never>,
+  never
+>;
 
-export type XOR<T, U> = (Without<T> & U) | (Without<U> & T);
+type XORCollectKeys<T extends any[], R> =
+  T["length"] extends 0
+    ? R
+    : XORCollectKeys<RemoveFirst<T>, keyof T[0] | R>;
+
+type RemoveFirst<T extends any[]> =
+  T extends [any, ...infer Rest] ? Rest : never;
+
+type XORBuild<
+  T extends any[],
+  K extends keyof any,
+  R
+> =
+  T["length"] extends 0
+    ? R
+    : XORBuild<
+      RemoveFirst<T>,
+      K,
+      | R
+      | (
+      // 현재 타입 T[0] 에 없는 키들을 모두 Optional + undefined 로 만들고
+      Partial<Record<Exclude<K, keyof T[0]>, undefined>>
+      // T[0] 과 합침
+      & T[0]
+      )
+    >;

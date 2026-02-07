84d154aac8a1e3b50b6087e6ded62b7950b20462
POWFIX <powfix@gmail.com>
2026-02-08 01:44:42 +0900

    renamed

--- src/browser/utils/SortUtils.ts
+++ src/browser/utils/SortUtils.ts
@@ -2,14 +2,14 @@ import {SortItem, SortUtils as BaseSortUtils} from "../../shared";
 import type {GridSortModel} from "@mui/x-data-grid";
 
 export class SortUtils extends BaseSortUtils {
-  static mapGridSortItem<TField extends string = string>(item: SortItem<TField>): GridSortModel[number] {
+  static toGridSortItem<TField extends string = string>(item: SortItem<TField>): GridSortModel[number] {
     return {
       field: item.field,
       sort: item.direction,
     };
   }
 
-  static mapGridSortModel<TField extends string = string>(items: SortItem<TField>[]): GridSortModel {
-    return items.map(this.mapGridSortItem);
+  static toGridSortModel<TField extends string = string>(items: SortItem<TField>[]): GridSortModel {
+    return items.map(this.toGridSortItem);
   }
 }
